1b25c4116dae5365c3a433ff0399eacfbab63548
keoo156 <someonewasthere75@gmail.com>
2023-04-15 11:11:56 +0800

    connection setting

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const { engine }= require("express-handlebars")
-const routes = require("./routes/")
+const routes = require("./routes")
+require("./config/mongoose")
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-mongoose.connect(process.env.MONGODB_URI)
-const db = mongoose.connection
-db.on("error",()=>{
-    console.log("error")
-})
-db.once("open", () =>{
-    console.log("success")
-})
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+//   }
+// mongoose.connect(process.env.MONGODB_URI)
+// const db = mongoose.connection
+// db.on("error",()=>{
+//     console.log("error")
+// })
+// db.once("open", () =>{
+//     console.log("success")
+// })
 
 
 app.engine("handlebars", engine({defaultLayout:"main"}))
